a170214b11b7be1d7fc8e44b7db2f7cf79283401
kenengk <123220622+kenengk@users.noreply.github.com>
2023-02-11 16:48:02 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,23 @@
-mui.alert("目前版本充值率较低，主要因为懒得在抓一个直冲值","公告")
-function w(){
-mui.alert("暂未开启");
-}
+//修改代码
+        let put1 = "LYZC_WC1";
+        let put2 = "LYZC_WC2";
+        let put3 = "LYZC_WC3";
+        let put4 = "LYZC_WC4";
+        let count = 0;
+        let now = new Date(2023, 0, 30, 17, 0, 0);
+        let tomorrow = new Date(now.getTime() + 24 * 60 * 60 * 1000);
+        function km1(){
+            let pwd1 = document.getElementById("pwd1").value;
+            if(pwd1 == put1 && now < tomorrow || pwd1 == put2 || pwd1 == put3 || pwd1 == put4)
+            {
+                mui.alert("卡密输入正确，请进入");
+                window.open("jrjjm.html");
+            }  else{
+                count++;
+                if(count>=3){
+                    mui.alert("输入次数过多，请稍后再试！","充值提示");  
+                            }else{
+                    mui.alert("卡密无效!","充值提示");  
+                                 }
+                            }
+                    }
